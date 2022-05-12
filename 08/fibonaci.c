846db86feb637cc2cdb0c235bf1cc6b391fbd8eb
Obradovic Nikola <obradovicnikola009@gmail.com>
2022-05-12 19:30:02 +0200

    fix lowest level return value

--- 08/fibonaci.c
+++ 08/fibonaci.c
@@ -54,33 +54,33 @@ int fibonaci(int pocetno){
 		}
 	}
 
-	printf("%i\n", i);
+	//printf("%i\n", i);
 	
 	if (i == 1){
-		printf("I'm bottom process.\n");
+		//printf("I'm bottom process.\n");
 		int task = 0;
 		int rez = 0;
 		read(citajOdRoditelja, &task, sizeof(int));
-		printf("Taking task %i\n", task);
+		printf("Nivo %i, taking task %i\n", i, task);
 		if(task == 1) rez = 1;
 		else rez = 0;
 		
 		printf("nivo - %i, zadatak %i, vracam rez %i\n", i, task, rez);
 		write(pisiRoditelju, &rez, sizeof(int));
 	} else if(i == pocetno){
-		printf("I'm top process.\n");
+		//printf("I'm top process.\n");
 		int rez = 999;
 		int task = i-1;
-		printf("Adding task %i\n", task);
+		printf("Nivo %i, adding task %i and %i\n", i, task, task-1);
 		write(pisiDetetu, &task, sizeof(int));
 		task--;
-		printf("Adding task %i\n", task);
+		//printf("Adding task %i\n", task);
 		write(pisiDetetu, &task, sizeof(int));
 
-		//while(wait(NULL) != -1){}
 		int rez2 = 0;
 		read(citajOdDeteta, &rez, sizeof(int));
 		read(citajOdDeteta, &rez2, sizeof(int));
+		printf("Nivo - %i, uzimam rezultate %i i %i\n", i, rez, rez2);
 		rez += rez2;
 
 		close(pisiDetetu);
@@ -91,20 +91,22 @@ int fibonaci(int pocetno){
 	} else {
 		int task = 0;
 		read(citajOdRoditelja, &task, sizeof(int));
-		printf("Taking task %i\n", task);
+		//printf("Taking task %i\n", task);
+		int tmp = task;
 
-		task--;
-		write(pisiDetetu, &task, sizeof(int));
-		task--;
-		write(pisiDetetu, &task, sizeof(int));
+		tmp--;
+		write(pisiDetetu, &tmp, sizeof(int));
+		tmp--;
+		write(pisiDetetu, &tmp, sizeof(int));
 
+		while(wait(NULL) != -1){}
 		int rez1 = 0, rez2 = 0;
 		read(citajOdDeteta, &rez1, sizeof(int));
-		printf("nivo - %i, uzimam od deteta %i\n", i, rez1);
 		read(citajOdDeteta, &rez2, sizeof(int));
-		printf("nivo - %i, uzimam od deteta %i\n", i, rez1);
-		task = rez1+rez2;
-		write(pisiRoditelju, &task, sizeof(int));
+		printf("nivo - %i, zadatak %i, uzimam od deteta %i i %i\n", i, task, rez1, rez2);
+		rez1 = rez1+rez2;
+		if(task==1) rez1 = 1;
+		write(pisiRoditelju, &rez1, sizeof(int));
 	}
 	
 	close(pisiDetetu);
@@ -112,6 +114,7 @@ int fibonaci(int pocetno){
 	close(pisiRoditelju);
 	close(citajOdRoditelja);
 	
+	while(wait(NULL) != -1){}
 	_exit(0);
 }
 
